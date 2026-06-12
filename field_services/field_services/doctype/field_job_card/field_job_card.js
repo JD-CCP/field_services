2a4667dabaaf46234488d4564f64bd101e7cc920
JD <jd@compucable.co.za>
2026-06-12 16:34:58 +0200

    feat: Add client sign-off section and multi-photo upload to Field Job Card
    - New Client Sign Off section at the bottom: signed-off-by name,
      contact number, email, 5-star satisfaction rating, draw-on-screen
      client signature, and auto-stamped sign-off date
    - sign_off_date is set automatically when a name or signature is
      entered, cleared if both are removed
    - 'Upload Photos' button opens the multi-file uploader (images only);
      each uploaded file is appended as a row in the photos table
    - Removed leftover custom autoname so the FJC-.YYYY.-.##### series
      from the doctype definition applies
    
    Co-Author: Fiskerton

---
+++ field_services/field_services/doctype/field_job_card/field_job_card.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2026, CompuCable Projects CC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Field Job Card", {
+	refresh: function (frm) {
+		frm.add_custom_button(__("Upload Photos"), function () {
+			if (frm.is_new()) {
+				frappe.msgprint(__("Please save the Job Card before uploading photos."));
+				return;
+			}
+
+			new frappe.ui.FileUploader({
+				doctype: frm.doctype,
+				docname: frm.docname,
+				folder: "Home/Attachments",
+				allow_multiple: true,
+				restrictions: {
+					allowed_file_types: ["image/*"],
+				},
+				on_success: function (file_doc) {
+					frm.add_child("photos", {
+						photo: file_doc.file_url,
+					});
+					frm.refresh_field("photos");
+					frm.dirty();
+				},
+			});
+		});
+	},
+});
--- field_services/field_services/doctype/field_job_card/field_job_card.py
+++ field_services/field_services/doctype/field_job_card/field_job_card.py
@@ -1,14 +1,14 @@
 import frappe
 from frappe.model.document import Document
-from frappe.model.naming import getseries
 from frappe.utils import now_datetime
 
 
 class FieldJobCard(Document):
-	def autoname(self):
-		if not self.project:
-			frappe.throw("Project is required to create a Field Job Card")
-		date_part = now_datetime().strftime("%y%m%d")
-		series_key = f"FJC-{self.project}-{date_part}"
-		counter = getseries(series_key, 1)
-		self.name = f"{self.project}-{date_part}-{counter}"
+	def validate(self):
+		self.set_sign_off_date()
+
+	def set_sign_off_date(self):
+		if (self.sign_off_name or self.client_signature) and not self.sign_off_date:
+			self.sign_off_date = now_datetime()
+		elif not self.sign_off_name and not self.client_signature:
+			self.sign_off_date = None
